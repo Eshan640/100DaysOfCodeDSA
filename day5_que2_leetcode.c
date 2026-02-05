f5fcef99194c634e6f3917e03161d2ea07d27d9c
Eshan Adlakha <eshanadlakha5@gmail.com>
2026-02-05 02:48:06 +0000

    leetcode

---
+++ day5_que2_leetcode.c
@@ -0,0 +1,20 @@
+void merge(int* nums1, int nums1Size, int m,
+           int* nums2, int nums2Size, int n) {
+
+    int i = m - 1;              // last valid in nums1
+    int j = n - 1;              // last in nums2
+    int k = nums1Size - 1;      // end of nums1 buffer
+
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] > nums2[j]) {
+            nums1[k--] = nums1[i--];
+        } else {
+            nums1[k--] = nums2[j--];
+        }
+    }
+
+    // copy remaining nums2
+    while (j >= 0) {
+        nums1[k--] = nums2[j--];
+    }
+}
